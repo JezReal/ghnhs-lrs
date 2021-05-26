25a2a8bc7f92f74b9733d011dae6f972bc1fe674
Jezreel Joshua <jezreeljoshuam@gmail.com>
2021-05-27 00:56:13 +0800

    clean up code

--- src/main/java/io/github/jezreal/App.java
+++ src/main/java/io/github/jezreal/App.java
@@ -15,7 +15,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("prototype/prototype.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("ui/home.fxml"));
 
         Parent root = fxmlLoader.load();
